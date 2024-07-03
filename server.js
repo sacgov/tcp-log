6740caccafe8a56ad299de915f3648aa7744bd08
shreeya <1603shreeyas@gmail.com>
2024-07-03 13:07:51 +0530

    add the files

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const processMessage = (connectionData) => {
 
 const startServer = () => {
   const sockInfo = {};
+
   sockInfo.listMessages = [];
   const server = net.createServer();
   server.listen(port, host, () => {
