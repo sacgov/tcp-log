a4fbdb2fd634b0e29db3fa8349097cbc8d199bb2
Sachin Govind <bssachingovind@gmail.com>
2024-07-03 17:58:46 +0530

    changes

---
+++ storeMessagesInDb.js
@@ -0,0 +1,17 @@
+const Messages = require('./db/message');
+
+const storeMessage = (parsedMessage) => {
+  Messages.create({
+    header: parsedMessage.header,
+    imei: parsedMessage.imei,
+    rawMessage: parsedMessage.rawMessage,
+    receivedTime: parsedMessage.received_time_moment,
+    error: parsedMessage.error,
+  }).catch((error) => {
+    console.error('Failed to create a new record : ', error);
+  });
+};
+
+module.exports = {
+  storeMessage,
+};
