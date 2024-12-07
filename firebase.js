0587f41e8fe100bd932e72003329429cb9d6410b
Sachin Govind <bssachingovind@gmail.com>
2024-12-07 11:05:40 +0530

    commit

--- firebase.js
+++ firebase.js
@@ -2,12 +2,20 @@ var admin = require('firebase-admin');
 const { getAuth } = require('firebase/auth');
 
 var serviceAccount = require('./epickbikes-personal.json');
+console.log(serviceAccount);
 
+console.log('verifying app');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+console.log('completed verifying app');
 
 const verifyToken = (idToken) => {
+  if (admin.apps.length === 0) {
+    console.log('app is not initialised');
+  } else {
+    console.log('app is  initialised');
+  }
   return getAuth()
     .verifyIdToken(idToken)
     .then((decodedToken) => {
@@ -20,6 +28,11 @@ const verifyToken = (idToken) => {
     });
 };
 
+const init = () => {
+  // dummy function to init this file with require
+};
+
 module.exports = {
   verifyToken,
+  init,
 };
